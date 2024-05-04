3e9f777d17606e3e18502d10b35962cb073a51de
GachaMations <gachamations1@gmail.com>
2024-05-04 23:32:35 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// const socket = new WebSocket("ws://127.0.0.1:3050/");socket.addEventListener('open', (event) => { socket.send("connect"); });
+
+using System.Net.Sockets;
+using System.Net;
+using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static Server;
+class Server
+{
+    private static WebSocketServer server;
+    public class Command
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = "connect";
+
+        [JsonPropertyName("data")]
+        public JsonElement Data { get; set; }
+    }
+    public class Message
+    {
+        [JsonPropertyName("char")]
+        public string Char { get; set; } = "";
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = "";
+
+        [JsonPropertyName("msg")]
+        public string Msg { get; set; } = "";
+    }
+    public class Socket : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            try
+            {
+                Command? cmd = JsonSerializer.Deserialize<Command>(e.Data);
+                switch (cmd.Type)
+                {
+                    case "message":
+                        server.WebSocketServices["/"].Sessions.Broadcast(cmd.Data.ToString());
+                        break;
+                    case "connect":
+                        Console.WriteLine("User Connected.");
+                        Send("Connected.");
+                        break;
+                    default:
+                        Send("Unknown Command.");
+                        break;
+                }
+            } catch (Exception ex)
+            {
+                Send("ERROR - " + ex.ToString());
+                Console.WriteLine("ERROR - " + ex.ToString());
+            }
+        }
+    }
+    public static void Main()
+    {
+        server = new WebSocketServer("ws://127.0.0.1:3050");
+        server.AddWebSocketService<Socket>("/");
+        server.Start();
+        Console.WriteLine("Server started", Environment.NewLine);
+        Console.ReadLine();
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Gacha Life Unity Chat Server
+Server used for the revived Chat feature from Gacha Life in Gacha Life Unity.
+Has to be hosted manually as of now, but i'll hopefully have an online server that the game connects to by default.
+You'll probably still be able to add custom servers on release, but it will default to the main chat server.
+
+Will probably be updated in the future for performance reasons, and to patch vulnerabilities.
